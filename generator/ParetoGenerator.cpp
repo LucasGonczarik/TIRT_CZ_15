7862b7ee4c8ca16e686f64b9592f2d984dc84e6a
dawhey <dawhey@gmail.com>
2017-05-26 12:18:51 +0200

    Initial branch commit

---
+++ generator/ParetoGenerator.cpp
@@ -0,0 +1,18 @@
+/*
+ * ParetoGenerator.cpp
+ *
+ *  Created on: 26.05.2017
+ *      Author: dawhe
+ */
+
+#include "ParetoGenerator.h"
+
+ParetoGenerator::ParetoGenerator() {
+}
+
+ParetoGenerator::~ParetoGenerator() {
+}
+
+Define_Module(ParetoGenerator);
+
+
---
+++ generator/ParetoGenerator.h
@@ -0,0 +1,17 @@
+/*
+ * ParetoGenerator.h
+ *
+ *  Created on: 26.05.2017
+ *      Author: dawhe
+ */
+
+#ifndef GENERATOR_PARETOGENERATOR_H_
+#define GENERATOR_PARETOGENERATOR_H_
+
+class ParetoGenerator : public BasicGenerator {
+public:
+    ParetoGenerator();
+    virtual ~ParetoGenerator();
+};
+
+#endif /* GENERATOR_PARETOGENERATOR_H_ */
